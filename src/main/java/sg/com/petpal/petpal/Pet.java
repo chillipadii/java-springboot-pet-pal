10959ee418c1b1fd1bdddfe75591d3b5fa63b680
limkeith <lim_keith@yahoo.com.sg>
2024-09-23 20:40:31 +0800

    Pets completed

---
+++ src/main/java/sg/com/petpal/petpal/Pet.java
@@ -0,0 +1,51 @@
+package sg.com.petpal.petpal;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+//@Builder
+@Entity
+@Table(name = "pets")
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender", nullable = false)
+    private Gender gender;
+
+    @Column(name = "age", nullable = false)
+    private Integer age;
+
+    @Column(name = "is_neutered", nullable = false)
+    private boolean isNeutered;
+
+    @ElementCollection
+    @Column(name = "pictures")
+    private List<String> pictures;
+
+    @Column(name = "description", length = 1000)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id", referencedColumnName = "id")
+    private Owner owner;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_data_id", referencedColumnName = "id")
+    private PetData petData;
+}
