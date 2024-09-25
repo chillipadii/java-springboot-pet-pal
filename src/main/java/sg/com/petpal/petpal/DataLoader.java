f7e82581aeaacd345e6f77591946aef0f2775713
Soh <dexsjw@gmail.com>
2024-09-25 17:58:52 +0800

    updated DataLoader to load chat test data

--- src/main/java/sg/com/petpal/petpal/DataLoader.java
+++ src/main/java/sg/com/petpal/petpal/DataLoader.java
@@ -3,6 +3,17 @@ package sg.com.petpal.petpal;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import sg.com.petpal.petpal.model.ChatMessage;
+import sg.com.petpal.petpal.model.ChatRoom;
+import sg.com.petpal.petpal.repository.ChatMessageRepository;
+import sg.com.petpal.petpal.repository.ChatRoomRepository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Component
@@ -11,11 +22,22 @@ public class DataLoader implements CommandLineRunner {
     //@Autowired
     //private OwnerRepository ownerRepository;
 
-    @Autowired
+    // @Autowired
     private PetRepository petRepository;
+    private ChatRoomRepository chatRoomRepository;
+    private ChatMessageRepository chatMessageRepository;
+
+    public DataLoader(PetRepository petRepository, 
+        ChatRoomRepository chatRoomRepository, ChatMessageRepository chatMessageRepository) {
+            this.petRepository = petRepository;
+            this.chatRoomRepository = chatRoomRepository;
+            this.chatMessageRepository = chatMessageRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
+
+        deleteAllExistingData();
         
         // Create the fist Pet (Buddy)
         Pet pet1 = new Pet();
@@ -54,5 +76,34 @@ public class DataLoader implements CommandLineRunner {
         //ownerRepository.save(owner);
         petRepository.save(pet3);
 
+        loadChatMessageData(loadChatRoomData());
     }
+
+    private void deleteAllExistingData() {
+        // petRepository.deleteAll();
+        chatRoomRepository.deleteAll();
+        chatMessageRepository.deleteAll();
+    }
+
+    private List<ChatRoom> loadChatRoomData() {
+        List<ChatRoom> chatRooms = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            ChatRoom newChatRoom = ChatRoom.builder().id(UUID.randomUUID()).build();
+            chatRooms.add(newChatRoom);
+        }
+        return chatRooms;
+    }
+
+    private void loadChatMessageData(List<ChatRoom> chatRooms) {
+        for (int i = 0; i < chatRooms.size(); i++) {
+            ChatMessage newChatMessage = ChatMessage.builder()
+                .id(UUID.randomUUID())
+                .timestamp(LocalDateTime.now())
+                .message("Chat Message " + i)
+                .chatRoomId(chatRooms.get(i))
+                .build();
+            chatMessageRepository.save(newChatMessage);
+        }
+    }
+
 }
