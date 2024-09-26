58f5996d0e183e555b43e54480cac73c24faec06
dexsjw <dextersjw@gmail.com>
2024-09-27 05:36:21 +0800

    added logic for ChatRoomController

--- src/main/java/sg/com/petpal/petpal/controller/ChatRoomController.java
+++ src/main/java/sg/com/petpal/petpal/controller/ChatRoomController.java
@@ -1,10 +1,55 @@
 package sg.com.petpal.petpal.controller;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import sg.com.petpal.petpal.model.ChatRoom;
+import sg.com.petpal.petpal.service.ChatRoomService;
 
 @RestController
 @RequestMapping("/chat/room")
 public class ChatRoomController {
+
+    private ChatRoomService chatRoomService;
+
+    public ChatRoomController(ChatRoomService chatRoomService) {
+        this.chatRoomService = chatRoomService;
+    }
+
+    @GetMapping({"", "/"})
+    public ResponseEntity<List<ChatRoom>> findAllChatRooms() {
+        return ResponseEntity.ok(chatRoomService.findAllChatRooms());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ChatRoom> findChatRoomById(@PathVariable UUID id) {
+        return ResponseEntity.ok(chatRoomService.findChatRoomById(id));
+    }
+
+    @PostMapping({"", "/"})
+    public ResponseEntity<ChatRoom> createChatRoom(@RequestBody ChatRoom chatRoom) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(chatRoomService.createChatRoom(chatRoom));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ChatRoom> updateChatRoomById(@PathVariable UUID id, @RequestBody ChatRoom chatRoom) {
+        return ResponseEntity.ok(chatRoomService.updateChatRoomById(id, chatRoom));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteChatRoomById(UUID id) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
     
 }
