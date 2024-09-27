99221786f4ac4c3d5942eaf1ccab1c7fae5ac1de
dexsjw <dextersjw@gmail.com>
2024-09-28 04:38:55 +0800

    completed ChatMessage API

--- src/main/java/sg/com/petpal/petpal/service/impl/ChatMessageServiceImpl.java
+++ src/main/java/sg/com/petpal/petpal/service/impl/ChatMessageServiceImpl.java
@@ -6,23 +6,42 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import sg.com.petpal.petpal.dto.ChatMessageOwnerDto;
 import sg.com.petpal.petpal.exception.ChatMessageNotFoundException;
+import sg.com.petpal.petpal.exception.ChatRoomNotFoundException;
+import sg.com.petpal.petpal.exception.NotChatRoomOwnerException;
+import sg.com.petpal.petpal.exception.OwnerNotFoundException;
 import sg.com.petpal.petpal.model.ChatMessage;
+import sg.com.petpal.petpal.model.ChatRoom;
+import sg.com.petpal.petpal.model.Owner;
 import sg.com.petpal.petpal.repository.ChatMessageRepository;
+import sg.com.petpal.petpal.repository.ChatRoomRepository;
+import sg.com.petpal.petpal.repository.OwnerRepository;
 import sg.com.petpal.petpal.service.ChatMessageService;
 
 @Service
 public class ChatMessageServiceImpl implements ChatMessageService {
 
+    private OwnerRepository ownerRepository;
+    private ChatRoomRepository chatRoomRepository;
     private ChatMessageRepository chatMessageRepository;
 
-    public ChatMessageServiceImpl(ChatMessageRepository chatMessageRepository) {
-        this.chatMessageRepository = chatMessageRepository;
+    public ChatMessageServiceImpl(OwnerRepository ownerRepository, 
+        ChatRoomRepository chatRoomRepository, 
+        ChatMessageRepository chatMessageRepository) {
+            this.ownerRepository = ownerRepository;
+            this.chatRoomRepository = chatRoomRepository;
+            this.chatMessageRepository = chatMessageRepository;
     }
 
     @Override
-    public List<ChatMessage> findAllChatMessages() {
-        return chatMessageRepository.findAll();
+    public List<ChatMessage> findChatRoomAllMessages(UUID chatRoomId) {
+        if (chatRoomId != null) {
+            ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow(() -> new ChatRoomNotFoundException(chatRoomId));
+            return chatRoom.getChatMessages();
+        } else {
+            return chatMessageRepository.findAll();
+        }
     }
 
     @Override
@@ -31,22 +50,33 @@ public class ChatMessageServiceImpl implements ChatMessageService {
     }
 
     @Override
-    public ChatMessage createChatMessage(ChatMessage chatMessage) {
-        chatMessage.setCreatedTimestamp(LocalDateTime.now());
-        chatMessage.setUpdatedTimestamp(LocalDateTime.now());
+    public ChatMessage createChatMessage(UUID chatRoomId, ChatMessageOwnerDto chatMessageOwnerDto) {
+        Long ownerId = chatMessageOwnerDto.getOwnerId();
+        Owner owner = ownerRepository.findById(ownerId).orElseThrow(() -> new OwnerNotFoundException(ownerId));
+        ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow(() -> new ChatRoomNotFoundException(chatRoomId));
+        if (!chatRoom.getOwners().contains(owner)) {
+            throw new NotChatRoomOwnerException(ownerId);
+        }
+        ChatMessage chatMessage = ChatMessage.builder()
+            .createdTimestamp(LocalDateTime.now())
+            .updatedTimestamp(LocalDateTime.now())
+            .message(chatMessageOwnerDto.getMessage())
+            .owner(owner)
+            .chatRoom(chatRoom)
+            .build();
         return chatMessageRepository.save(chatMessage);
     }
 
     @Override
-    public ChatMessage updateChatMessageById(UUID id, ChatMessage chatMessage) {
+    public ChatMessage updateChatMessageById(UUID id, ChatMessageOwnerDto chatMessageOwnerDto) {
         ChatMessage chatMessageToUpdate = chatMessageRepository.findById(id).orElseThrow(() -> new ChatMessageNotFoundException(id));
         chatMessageToUpdate.setUpdatedTimestamp(LocalDateTime.now());
-        chatMessageToUpdate.setMessage(chatMessage.getMessage());
+        chatMessageToUpdate.setMessage(chatMessageOwnerDto.getMessage());
         return chatMessageRepository.save(chatMessageToUpdate);
     }
 
     @Override
-    public void deleteChatMessageById(UUID id) {
+    public void deleteChatMessageById( UUID id) {
         chatMessageRepository.deleteById(id);
     }
     
