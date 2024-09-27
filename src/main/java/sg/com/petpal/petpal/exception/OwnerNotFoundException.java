08938274ca050201bc5e5f25e46fc6e41bc0c45d
dexsjw <dextersjw@gmail.com>
2024-09-28 01:46:43 +0800

    added public access to OwnerNotFoundException

--- src/main/java/sg/com/petpal/petpal/exception/OwnerNotFoundException.java
+++ src/main/java/sg/com/petpal/petpal/exception/OwnerNotFoundException.java
@@ -1,7 +1,7 @@
 package sg.com.petpal.petpal.exception;
 
 public class OwnerNotFoundException extends RuntimeException {
-    OwnerNotFoundException(Long id) {
+    public OwnerNotFoundException(Long id) {
         super("Could not find owner with id: " + id);
     }
 }
